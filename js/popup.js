048fb9121c91aa9ddcf8b0744cb61d5ca7af28cd
Dan-ee-Wang <56665526+Dan-ee-Wang@users.noreply.github.com>
2023-04-07 22:53:56 -0500

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,33 @@
-alert('Hello, World!');
+const mysql = require('mysql2/promise');
+
+async function createTable() {
+  // create a connection to the database
+  const connection = await mysql.createConnection({
+  host: 'c5mnq4bhkznq.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'password',
+  database: 'database-connection-test'
+  });
+
+  // define the SQL query to create the table
+  const sql = `
+    CREATE TABLE my_table (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(50),
+      age INT,
+      email VARCHAR(255)
+    )
+  `;
+
+  // execute the query to create the table
+  await connection.execute(sql);
+
+  alert('Table created successfully.');
+
+  // close the database connection
+  await connection.end();
+}
+
+createTable().catch((error) => {
+  alert('error');
+});
